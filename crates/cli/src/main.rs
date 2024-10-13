126588a59ed2971c156f9c36ea2861cf924b2b38
Lucas Meier <lucas@cronokirby.com>
2024-10-13 03:35:39 -0700

    Implement basic scaffolding of crates
    We define three crates:
    1. "spine",
    2. "web",
    3. "cli".
    
    The "spine" crate is for the core logic and data fetching, agnostic
    of presentation.
    
    The "cli" crate is for retrieval from the command line.
    
    The "web" crate is for presenting a web frontend.

---
+++ crates/cli/src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("{}", numi_spine::MESSAGE);
+}
---
+++ crates/spine/src/lib.rs
@@ -0,0 +1,2 @@
+/// A static message to test integration with other libraries.
+pub const MESSAGE: &'static str = "Hello From Numi!";
